6b3db9c9cf8a82a0fbf0defc20e8d637941ffa66
mikramarc <michal@shadowrobot.com>
2020-04-01 11:12:42 +0000

    adding pose tests

---
+++ sr_pose_tests/scripts/test_hand_poses.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# Copyright 2019 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import rospy
+import rospkg
+from sr_run_trajectories.run_trajectories import SrRunTrajectories
+
+if __name__ == "__main__":
+    rospy.init_node('run_hand_poses')
+    trajectories_file_path = rospkg.RosPack().get_path('sr_pose_tests') + '/config/tested_hand_poses.yaml'
+    srt = SrRunTrajectories(trajectories_file_path, arm=False)
+    for pose in srt._hand_trajectories:
+        raw_input("About to go to pose {}. Press [RETURN] to execute...".format(pose))
+        srt.run_trajectory('hand', pose)
+    rospy.loginfo("All poses have been tested. Exiting.")
