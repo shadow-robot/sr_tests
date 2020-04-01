c4a31dd852c3807d7f168d3eb1890d0859ecd7a7
mikramarc <michal@shadowrobot.com>
2020-04-01 15:12:28 +0000

    parametrizing for hand type

--- sr_pose_tests/scripts/test_hand_poses.py
+++ sr_pose_tests/scripts/test_hand_poses.py
@@ -20,8 +20,20 @@ from sr_run_trajectories.run_trajectories import SrRunTrajectories
 
 if __name__ == "__main__":
     rospy.init_node('run_hand_poses')
-    trajectories_file_path = rospkg.RosPack().get_path('sr_pose_tests') + '/config/hand_poses_to_test.yaml'
+    hand_type = rospy.get_param('~hand_type', 'hand_e')
+    biotac = rospy.get_param('~biotac', False)
+
+    # TODO: Extend with more hand types if necessary
+    if 'hand_e' == hand_type:
+        poses_yaml_file_name = 'hand_poses_to_test_hand_e'
+        if biotac:
+            poses_yaml_file_name += '_biotac'
+    else:
+        raise ValueError("Unknown hand type!")
+
+    trajectories_file_path = rospkg.RosPack().get_path('sr_pose_tests') + '/config/{}.yaml'.format(poses_yaml_file_name)
     srt = SrRunTrajectories(trajectories_file_path, arm=False)
+
     for pose in srt._hand_trajectories:
 	if 'open' == pose:
 	    continue
